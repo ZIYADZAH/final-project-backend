c61d4c715af4536ebdda6de83ad3910f41cad686
ziyad <ziadzaher2525@gmail.com>
2023-11-30 05:37:36 -0500

    add

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const add = (0, express_1.default)();
 const PORT = 3000;
+add.get("/ziyad", function (req, res) {
+    res.send("hello word.");
+});
 add.listen(PORT, () => {
     console.log(`server 3000 ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const PORT =3000;
 add.get("/ziyad",function(req , res){
     res.send("hello word.")
 });
-
+// 
 add.listen(PORT, ()=>{   
 console.log(`server 3000 ${PORT}`);
 
